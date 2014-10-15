3b7a3d230836a2122250fb522bdca4eae67be201
Saydd Salas <gio_vela10@hotmail.com>
2014-10-15 17:22:57 -0600

    adding again controllers

---
+++ public/js/controllers.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp', []);
+
+app.controller('myController', function($scope){
+	$scope.author = {
+    'name' : 'Saydd Salas',
+    'title' : 'Frontend developer',
+    'company' : 'innova web'$scope$scope$scope$scope$scope
+  }
+});
